c35b88f2180c157394776a674f5b67e14521ed85
Jakub Oboza <jakub.oboza@gmail.com>
2014-05-15 09:54:12 +0100

    added ability to set custom colours to strings

--- src/test/scala/pl/project13/scala/rainbow/ImplicitRainbowSpec.scala
+++ src/test/scala/pl/project13/scala/rainbow/ImplicitRainbowSpec.scala
@@ -6,11 +6,20 @@ class ImplicitRainbowSpec extends FlatSpec with Matchers {
 
   behavior of "Rainbow"
 
+  it should "be able to set custom colour" in {
+     "custom".colour("\033[30m") shouldEqual (Console.BLACK + "custom" + Console.RESET)
+  }
+
   it should "colorize strings" in {
     val red = "red".red
     val blue = "blue".blue
     val yellow = "yellow".yellow
     val green = "green".green
+
+    red shouldEqual (Console.RED + "red" + Console.RESET)
+    blue shouldEqual (Console.BLUE + "blue" + Console.RESET)
+    yellow shouldEqual (Console.YELLOW + "yellow" + Console.RESET)
+    green shouldEqual (Console.GREEN + "green" + Console.RESET)
 
     println("red = " + red)
     println("blue = " + blue)
