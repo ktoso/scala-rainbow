d50383245409096785f9719178a62aa7baebe41d
Tommey <github@tommey.hu>
2015-09-11 15:24:02 +0200

    Fixed comments and formatting
    Modified colors in comments to match the actual method's purpose
    Minor formatting fixes

--- src/main/scala/pl/project13/scala/rainbow/Rainbow.scala
+++ src/main/scala/pl/project13/scala/rainbow/Rainbow.scala
@@ -25,35 +25,35 @@ trait Rainbow {
     def black = BLACK + s + RESET
     /** Colorize the given string foreground to ANSI red */
     def red = RED + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI green */
     def green = GREEN + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI yellow */
     def yellow = YELLOW + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI blue */
     def blue = BLUE + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI magenta */
     def magenta = MAGENTA + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI cyan */
     def cyan = CYAN + s + RESET
-    /** Colorize the given string foreground to ANSI red */
+    /** Colorize the given string foreground to ANSI white */
     def white = WHITE + s + RESET
 
-    /** Colorize the given string background to ANSI red */
+    /** Colorize the given string background to ANSI black */
     def onBlack = BLACK_B + s + RESET
     /** Colorize the given string background to ANSI red */
-    def onRed = RED_B+ s + RESET
-    /** Colorize the given string background to ANSI red */
-    def onGreen = GREEN_B+ s + RESET
-    /** Colorize the given string background to ANSI red */
+    def onRed = RED_B + s + RESET
+    /** Colorize the given string background to ANSI green */
+    def onGreen = GREEN_B + s + RESET
+    /** Colorize the given string background to ANSI yellow */
     def onYellow = YELLOW_B + s + RESET
-    /** Colorize the given string background to ANSI red */
-    def onBlue = BLUE_B+ s + RESET
-    /** Colorize the given string background to ANSI red */
+    /** Colorize the given string background to ANSI blue */
+    def onBlue = BLUE_B + s + RESET
+    /** Colorize the given string background to ANSI magenta */
     def onMagenta = MAGENTA_B + s + RESET
-    /** Colorize the given string background to ANSI red */
-    def onCyan = CYAN_B+ s + RESET
-    /** Colorize the given string background to ANSI red */
-    def onWhite = WHITE_B+ s + RESET
+    /** Colorize the given string background to ANSI cyan */
+    def onCyan = CYAN_B + s + RESET
+    /** Colorize the given string background to ANSI white */
+    def onWhite = WHITE_B + s + RESET
 
     /** Make the given string bold */
     def bold = BOLD + s + RESET
